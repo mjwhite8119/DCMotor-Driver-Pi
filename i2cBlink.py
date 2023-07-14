87911612de157c98a0ae73ed5d05e8018dff5a45
Martin White <11601920+mjwhite8119@users.noreply.github.com>
2023-07-13 18:18:54 -0700

    added read functions

--- i2cBlink.py
+++ i2cBlink.py
@@ -1,6 +1,6 @@
-#  Raspberry Pi Master for Arduino Slave
+#  Raspberry Pi Master for ESP32 Slave
 #  i2c_master_pi.py
-#  Connects to Arduino via I2C
+#  Connects to ESP32 via I2C
   
 #  DroneBot Workshop 2019
 #  https://dronebotworkshop.com
@@ -14,6 +14,29 @@ data_to_send_to_ESP32 = "Hello ESP32"
 
 numb = 1
 
+def readMessage():
+    global smsMessage
+    data_received_from_ESP32 = bus.read_i2c_block_data(addr, 0,15)
+    for i in range(len(data_received_from_ESP32)):
+        smsMessage += chr(data_received_from_ESP32[i])
+
+    print(smsMessage.encode('utf-8'))
+    data_received_from_ESP32 =""
+    smsMessage = ""
+
+def readNumber():
+    global smsNumber
+    data_received_from_ESP32 = bus.read_i2c_block_data(addr, 0,15)
+    for i in range(len(data_received_from_ESP32)):
+        smsNumber += chr(data_received_from_ESP32[i])
+
+    print(smsNumber.encode('utf-8'))
+    data_received_from_ESP32 = ""
+    smsNumber = ""
+
+smsMessage = ""
+smsNumber = ""
+
 def StringToBytes(val):
     retVal = []
     for c in val:
@@ -25,15 +48,17 @@ while numb == 1:
 
 	ledstate = input(">>>>   ")
 
-	if ledstate == "1":
-		bus.write_byte(addr,3)
-		print(StringToBytes(data_to_send_to_ESP32))
-		bus.write_i2c_block_data(addr, 0x00,StringToBytes(data_to_send_to_ESP32))
-            
-		# bus.write_byte(addr, 0x1) # switch it on
-	elif ledstate == "0":
-		data_received_from_ESP32 = bus.read_i2c_block_data(addr, 0,12)
-		print(data_received_from_ESP32)
-		# bus.write_byte(addr, 0x0) # switch it on
-	else:
-		numb = 0
+  if ledstate == "1":
+    bus.write_byte(addr,3)
+    print(StringToBytes(data_to_send_to_ESP32))
+    bus.write_i2c_block_data(addr, 0x00,StringToBytes(data_to_send_to_ESP32))
+          
+  # bus.write_byte(addr, 0x1) # switch it on
+  elif ledstate == "0":
+    # data_received_from_ESP32 = bus.read_i2c_block_data(addr, 0,12)
+    readMessage()
+    # print(data_received_from_ESP32)
+              
+  # bus.write_byte(addr, 0x0) # switch it on
+  else:
+    numb = 0
