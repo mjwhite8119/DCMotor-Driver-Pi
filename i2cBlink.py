8dcc364a84bfe37be22b34d5aecfbefab85c3f5a
Martin <11601920+mjwhite8119@users.noreply.github.com>
2023-07-17 10:11:33 -0700

    added numpy

--- i2cBlink.py
+++ i2cBlink.py
@@ -6,14 +6,30 @@
 #  https://dronebotworkshop.com
 
 from smbus import SMBus
+import struct
+import numpy as np
+from shmem_buffer import *
+
 
 addr = 0x55 # Client address
 bus = SMBus(1) # indicates /dev/ic2-1
 BUFFER_LENGTH = 37
-data_received_from_ESP32 = ""
-data_to_send_to_ESP32 = "Hello ESP32"
+# data_received_from_ESP32 = ""
+
+data =  Data()
+motors = np.array([0,0,0,0,0,0], dtype='int8') 
+encoderResets = np.array([0,0,0,0,0,0], dtype='int8') 
+encoders = np.array([0,0,0,0,0,0], dtype='int16') 
 
 numb = 1
+
+def bytes(i):
+    #  return divmod(i, 0x100)
+    high, low = divmod(i, 0x100)
+    return [high, low]
+
+def int16(n):
+      return struct.unpack('H', struct.pack('h', n))
 
 def readMessage():
     global smsMessage
@@ -49,21 +65,50 @@ def StringToBytes(val):
 print ("Enter 1 for ON or 0 for OFF")
 while numb == 1:
 
-	ledstate = input(">>>>   ")
+    ledstate = input(">>>>   ")
 
-	if ledstate == "1":
-		bus.write_byte(addr,3)
-		print(StringToBytes(data_to_send_to_ESP32))
-		bus.write_i2c_block_data(addr, 0x00,StringToBytes(data_to_send_to_ESP32))
-			
-	# bus.write_byte(addr, 0x1) # switch it on
-	elif ledstate == "0":
-		data_received_from_ESP32 = bus.read_i2c_block_data(addr, 0, BUFFER_LENGTH)
+    if ledstate == "1":
+        motors[0] = 100
+        # high, low = bytes(buffer.motor1)
+        motors[1] = -90
+        motors[5] = 38
+        encoders[0] = 1045
+        encoders[1] = 3445
+        encoders[5] = 512
+ 
+        high1, low1 = bytes(encoders[0])
+        high2, low2 = bytes(encoders[1])
+        high3, low3 = bytes(encoders[2])
+        high4, low4 = bytes(encoders[3])
+        high5, low5 = bytes(encoders[4])
+        high6, low6 = bytes(encoders[5])
+        print(high1)
+        print(low1)
+        expanded_encoders = np.concatenate((bytes(encoders[0]),bytes(encoders[1]),bytes(encoders[2]),
+                                            bytes(encoders[3]),bytes(encoders[4]),bytes(encoders[5])), axis=None)
+
+        print(expanded_encoders)
+        array_buffer = np.concatenate((motors, encoderResets, expanded_encoders), axis=None)
+        print(array_buffer)
+        data_to_send_to_ESP32 = array_buffer.tolist()
+        # data_to_send_to_ESP32 = packBuffer(data)
+        # high1, low1 = bytes(buffer.motor2)
+
+        # data_to_send_to_ESP32 = [high, low, high1, low1]
+        # bus.write_byte(addr,3)
+        print(data_to_send_to_ESP32)
+        bus.write_i2c_block_data(addr, 0x00, data_to_send_to_ESP32)
+    # bus.write_byte(addr, 0x1) # switch it on
+    elif ledstate == "0":
+        # readNumber()
+        data_received_from_ESP32 = bus.read_i2c_block_data(addr, 0, 32)
         print(data_received_from_ESP32)
-        buffer(data_received_from_ESP32)
-
-		# readMessage()
-				
-	# bus.write_byte(addr, 0x0) # switch it on
-	else:
-		numb = 0
+        # string = ''.join([hex(item) for item in data_received_from_ESP32])
+        # print(string)
+        motor1 = data_received_from_ESP32[0:2]
+        print(motor1)
+        # readMessage()
+                
+    # bus.write_byte(addr, 0x0) # switch it on
+    else:
+        numb = 0
--- shmem_buffer.py
+++ shmem_buffer.py
@@ -1,25 +1,48 @@
+import struct
+import numpy as np
+
+
+def int16(n):
+      return struct.unpack('H', struct.pack('h', n))
+
 class Data:
-  def __init__(self, shmem_buffer):
-    self.buffer = buffer(shmem_buffer)
-  # uint8_t firmwareIdent;
-  # uint8_t status;
-  # bool heartbeat;
-  # int16_t Motor1;
-  # int16_t Motor2;
-  # int16_t Motor3;
-  # int16_t Motor4;
-  # int16_t Motor5;
-  # int16_t Motor6;
-  # uint16_t batteryMillivolts;
-  # bool resetEncoder1;
-  # bool resetEncoder2;
-  # bool resetEncoder3;
-  # bool resetEncoder4;
-  # bool resetEncoder5;
-  # bool resetEncoder6;
-  # int16_t Encoder1;
-  # int16_t Encoder2;
-  # int16_t Encoder3;
-  # int16_t Encoder4;
-  # int16_t Encoder5;
-  # int16_t Encoder6;
+  def __init__(self):
+     
+    motors = np.array([0,0,0,0,0,0], dtype='int8') 
+    encoderResets = np.array([0,0,0,0,0,0], dtype='int8') 
+    encoders = np.array([0,0,0,0,0,0], dtype='int16') 
+    # motor1 = 0 
+    # motor2 = 0
+    # motor1 = 0 
+    # motor2 = 0
+    # motor1 = 0 
+    # motor2 = 0
+    # resetEncoder1 = False 
+    # resetEncoder2 = False
+    # resetEncoder3 = False
+    # resetEncoder4 = False
+    # resetEncoder5 = False
+    # resetEncoder6 = False
+    # encoder1 = int16(0)
+    # encoder2 = int16(0)
+    # encoder3 = int16(0)
+    # encoder4 = int16(0)
+    # encoder5 = int16(0)
+    # encoder6 = int16(0)
+
+def packBuffer(data):
+    high1, low1 = bytes(data.encoder1)
+    high2, low2 = bytes(data.encoder2)
+    high3, low3 = bytes(data.encoder3)
+    high4, low4 = bytes(data.encoder4)
+    high5, low5 = bytes(data.encoder5)
+    high6, low6 = bytes(data.encoder6)
+
+    buffer = np.concatenate((data.motors, data.encoderResets, data.encoders,), axis=None)
+    #   buffer = [data.motor1, data.motor2, data.motor3, 
+    #             data.motor4, data.motor5, data.motor6,
+    #             data.resetEncoder1, data.resetEncoder2, data.resetEncoder3,
+    #             data.resetEncoder4, data.resetEncoder5, data.resetEncoder6,
+    #             high1, low1, high2, low2, high3, low3,
+    #             high4, low4, high5, low5, high6, low6] 
+    return buffer
