02ff4579cfe0a74aaae62e3965a304457cfbfece
Martin <11601920+mjwhite8119@users.noreply.github.com>
2023-08-09 12:15:49 -0700

    CANBus working!

--- CAN/send.py
+++ CAN/send.py
@@ -5,12 +5,12 @@ import time
 os.system('sudo ip link set can0 type can bitrate 500000')
 os.system('sudo ifconfig can0 up')
 
-can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan')# socketcan_native
 try:
     while True:
-        msg = can.Message(arbitration_id=0x123, data=[0, 1, 2, 3, 4, 5, 6, 7], is_extended_id=False)
-        can0.send(msg)
-        time.sleep(0.1)
+        with can.interface.Bus(channel = 'can0', bustype = 'socketcan') as can0:
+            msg = can.Message(arbitration_id=0x123, data=[0, 1, 2, 3, 4, 5, 6, 7], is_extended_id=False)
+            can0.send(msg)
+            time.sleep(1.0)
 
 except KeyboardInterrupt:
     os.system('sudo ifconfig can0 down')
